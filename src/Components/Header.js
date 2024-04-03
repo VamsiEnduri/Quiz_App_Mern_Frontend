9d65ee80964d26545f7dcbac499ac638adfef985
VamsiEnduri <vamsienduri1310@gmail.com>
2024-04-03 16:07:53 +0530

    just project started

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "../assets/logo.jpg";
+const Header = () => {
+  return (
+    <div>
+      <img src={logo} alt="" className="w-17 h-14 rounded-lg ml-3" />
+    </div>
+  );
+};
+
+export default Header;
